65b1e9be1edd9bf4032994016c7af6465e7184d4
Adrian Amaral <adri13amaral@gmail.com>
2023-02-28 15:40:11 -0300

    commit com metade do projeto, codando feed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import ProtectedRouter from './Components/Helper/ProtectedRouter';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import User from './User/User';
+import { UserStorage } from './UserContext';
 
 function App() {
-  return <div>App react</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRouter>
+                  <User />
+                </ProtectedRouter>
+              }
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
